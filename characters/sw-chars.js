cee4e81a62bada20818afbc28e109fdd3f9c5571
Kylee Harward <kyleeharward21@gmail.com>
2022-03-17 12:42:49 -0600

    pulled function from utility folder

--- characters/sw-chars.js
+++ characters/sw-chars.js
@@ -1,32 +1,36 @@
-import { people } from '../data/people.js'
+import { people } from "../data/people.js";
+import { getLastNumber, removeChildren} from "../utils/index.js";
 
-const header = document.querySelector('header')
-const main = document.querySelector('main')
+const header = document.querySelector("header");
+const main = document.querySelector("main");
 
-const allCharsButton = document.createElement('button')
-allCharsButton.textContent = 'All Characters'
-allCharsButton.addEventListener('click', function () {
-  console.log('Thanks for clicking!')
-  populateDOM()
-})
-header.appendChild(allCharsButton)
+const allCharsButton = document.createElement("button");
+allCharsButton.textContent = "All Characters";
+allCharsButton.addEventListener("click", function () {
+  populateDOM(people);
+});
 
-function populateDOM() {
-  people.forEach((person) => {
-    const personFig = document.createElement('figure')
-    const personImg = document.createElement('img')
-    let charNum = getLastNumber(person.url)
-    personImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
-    const personCap = document.createElement('figcaption')
-    personCap.textContent = 'Some star wars character'
+const maleCharacters = people.filter((person) => person.gender == "male");
 
-    personFig.appendChild(personImg)
-    personFig.appendChild(personCap)
-    main.appendChild(personFig)
-  })
-}
+const maleCharsButton = document.createElement("button");
+maleCharsButton.textContent = "Male Characters";
+maleCharsButton.addEventListener("click", () => populateDOM(maleCharacters));
 
-function getLastNumber(url) {
-  const secondToLastLetterOfUrl = url[url.length - 2]
-  return secondToLastLetterOfUrl// return the second to last number from the url property of a film object
+header.appendChild(allCharsButton);
+header.appendChild(maleCharsButton);
+
+function populateDOM(characters) {
+  removeChildren(main)
+  characters.forEach((person) => {
+    const personFig = document.createElement("figure");
+    const personImg = document.createElement("img");
+    let charNum = getLastNumber(person.url);
+    personImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`;
+    const personCap = document.createElement("figcaption");
+    personCap.textContent = person.name;
+
+    personFig.appendChild(personImg);
+    personFig.appendChild(personCap);
+    main.appendChild(personFig);
+  });
 }
