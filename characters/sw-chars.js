d838cad89559c631478a30837a08b1ccab7acfbf
Kylee Harward <kyleeharward21@gmail.com>
2022-03-17 21:05:17 -0600

    added female and other genders buttons

--- characters/sw-chars.js
+++ characters/sw-chars.js
@@ -1,5 +1,5 @@
 import { people } from "../data/people.js";
-import { getLastNumber, removeChildren} from "../utils/index.js";
+import { getLastNumber, removeChildren } from "../utils/index.js";
 
 const header = document.querySelector("header");
 const main = document.querySelector("main");
@@ -11,16 +11,29 @@ allCharsButton.addEventListener("click", function () {
 });
 
 const maleCharacters = people.filter((person) => person.gender == "male");
-
 const maleCharsButton = document.createElement("button");
 maleCharsButton.textContent = "Male Characters";
 maleCharsButton.addEventListener("click", () => populateDOM(maleCharacters));
 
+const femaleCharacters = people.filter((person) => person.gender == "female");
+const femaleCharsButton = document.createElement("button");
+femaleCharsButton.textContent = "Female Characters";
+femaleCharsButton.addEventListener("click", () =>
+  populateDOM(femaleCharacters)
+);
+
+const otherCharacters = people.filter((person) =>person.gender != "male" && person.gender != "female");
+const otherCharsButton = document.createElement("button");
+otherCharsButton.textContent = "Other Characters";
+otherCharsButton.addEventListener("click", () => populateDOM(otherCharacters));
+
 header.appendChild(allCharsButton);
 header.appendChild(maleCharsButton);
+header.appendChild(femaleCharsButton);
+header.appendChild(otherCharsButton);
 
 function populateDOM(characters) {
-  removeChildren(main)
+  removeChildren(main);
   characters.forEach((person) => {
     const personFig = document.createElement("figure");
     const personImg = document.createElement("img");
