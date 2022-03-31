458bec9eac8ae394530dcd36c1dbaf895d0cc192
Kylee Harward <kyleeharward21@gmail.com>
2022-03-31 10:12:26 -0600

    added own modal. mostly functioning

--- starships/sw-starships.js
+++ starships/sw-starships.js
@@ -1,46 +1,55 @@
-import { starships } from "../data/starships.js"
-import { removeChildren, getLastNumber } from "../utils/index.js"
+import { starships } from "../data/starships.js";
+import { removeChildren, getLastNumber } from "../utils/index.js";
 
-const nav = document.querySelector('.nav')
-const navList = document.querySelector('.navList')
-const shipViewer = document.querySelector('.shipViewer')
+const nav = document.querySelector(".nav");
+const navList = document.querySelector(".navList");
+const shipViewer = document.querySelector(".shipViewer");
 
-const modal = document.querySelector('.modal')
-const closeButton = document.querySelector('.modal-close')
-// TODO: add a click event listener for the modal-background div as well
-const shipMessage = document.querySelector('.box p')
-
-closeButton.addEventListener('click', () => modal.classList.toggle('is-active'))
+const shipMessage = document.querySelector(".modal p");
+// Get the modal
+var modal = document.getElementById("myModal");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
 
 function populateNav() {
   starships.forEach((starship) => {
-    const listItem = document.createElement('li')
-    
-    const anchor = document.createElement('a')
-    anchor.href = '#'
-    anchor.textContent = starship.name
-    anchor.addEventListener('click', () => populateShipView(starship))
+    const listItem = document.createElement("li");
 
-    listItem.appendChild(anchor)
-    navList.appendChild(listItem)
-  })
+    const anchor = document.createElement("a");
+    anchor.href = "#!";
+    anchor.textContent = starship.name;
+    anchor.addEventListener("click", () => populateShipView(starship));
+  
+    listItem.appendChild(anchor);
+    navList.appendChild(listItem);
+  });
 }
 
-populateNav()
+populateNav();
 
 function populateShipView(shipData) {
-  removeChildren(shipViewer)
+  removeChildren(shipViewer);
 
-  const shipImage = document.createElement('img')
-  let shipNum = getLastNumber(shipData.url)
-  shipImage.src = `https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`
-  shipImage.addEventListener('error', () => {
-    console.log("Image error!!!!!!")
-    shipImage.hidden = true
-    shipMessage.textContent = `The ship known as ${shipData.name} is currently in space port for repairs.`
-    modal.classList.toggle('is-active')
-  })
+  const shipImage = document.createElement("img");
+  let shipNum = getLastNumber(shipData.url);
+  shipImage.src = `https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`;
+  shipImage.addEventListener("error", () => {
+    console.log("Image error!!!!!!");
+    shipImage.hidden = true;
+    shipMessage.textContent = `The ship known as ${shipData.name} is currently in space port for repairs.`;
+    // When the user clicks the button, open the modal
+    modal.style.display = "block";
+  });
 
-  shipViewer.appendChild(shipImage)
-
+  shipViewer.appendChild(shipImage);
 }
