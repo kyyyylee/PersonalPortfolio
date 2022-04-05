8e0eb4392d57c8bf00a39b4a344084cf45600189
Kylee Harward <kyleeharward21@gmail.com>
2022-04-05 11:53:43 -0600

    congress from class

--- congress/congress.js
+++ congress/congress.js
@@ -1,11 +1,12 @@
-import { senators } from "../data/senators.js";
+import { senators } from '../data/senators.js'
 
-const senatorsDiv = document.querySelector(".senatorsDiv");
-const seniorityHeader = document.querySelector('p')
+const senatorsDiv = document.querySelector('.senatorsDiv')
+const seniorityHeader = document.querySelector('.seniority')
+const loyaltyList = document.querySelector('.loyaltyList')
 
-function simpleSenators() {
-  return senators.map((senator) => {
-    const middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `;
+function simplifiedSenators() {
+  return senators.map(senator => {
+    const middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `
     return {
       id: senator.id,
       name: `${senator.first_name}${middleName}${senator.last_name}`,
@@ -15,29 +16,44 @@ function simpleSenators() {
       seniority: +senator.seniority,
       state: senator.state,
       missedVotesPct: senator.missed_votes_pct,
-      loyaltyPct: senator.votes_with_party_pct,
-    };
-  });
+      loyaltyPct: senator.votes_with_party_pct
+    }
+  })
 }
 
-function populateSenatorDiv(simpleSenators) {
-  simpleSenators.forEach((senator) => {
-    const senatorFig = document.createElement("figure");
-    const senatorImg = document.createElement("img");
-    const senatorCap = document.createElement("figcaption");
+function populateSenatorDiv(senatorsArray) {
+  senatorsArray.forEach(senator => {
+    const senFigure = document.createElement('figure')
+    const figImg = document.createElement('img')
+    const figCaption = document.createElement('figcaption')
 
-    senatorImg.src = senator.imgURL;
-    senatorCap.textContent = senator.name;
+    figImg.src = senator.imgURL
+    figCaption.textContent = senator.name
 
-    senatorFig.appendChild(senatorImg);
-    senatorFig.appendChild(senatorCap);
-    senatorsDiv.appendChild(senatorFig);
-  });
-  //TODO: create figure element with image and figcaption
-  // set the image source to imgURL
-  // append children to the DOM
+    senFigure.appendChild(figImg)
+    senFigure.appendChild(figCaption)
+    senatorsDiv.appendChild(senFigure)
+  })
 }
-populateSenatorDiv(simpleSenators());
 
-const mostSeniorMember=simpleSenators().reduce((acc, senator) => acc.seniority > senator.seniority ? acc : senator)
-seniorityHeader.textContent= `The most senior senator is ${mostSeniorMember.name}`
+populateSenatorDiv(simplifiedSenators())
+
+const mostSeniorMember = simplifiedSenators().reduce((acc, senator) => acc.seniority > senator.seniority ? acc : senator)
+
+const biggestMissedVotesPct = simplifiedSenators().reduce((acc, senator) => acc.missedVotesPct > senator.missedVotesPct ? acc : senator)
+
+// console.log(biggestMissedVotesPct.missedVotesPct)
+
+const biggestVacationerList = simplifiedSenators().filter(senator => senator.missedVotesPct === biggestMissedVotesPct.missedVotesPct)
+
+console.log(biggestVacationerList)
+
+seniorityHeader.textContent = `The most senior Senator is ${mostSeniorMember.name} and the biggest fans of vacations are TBD...`
+
+simplifiedSenators().forEach(senator => {
+  if(senator.loyaltyPct === 100) {
+    let listItem = document.createElement('li')
+    listItem.textContent = senator.name
+    loyaltyList.appendChild(listItem)
+  }
+})
