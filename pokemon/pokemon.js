c00fb716043cf10624e8ad352441edbf6e493b3c
Kylee Harward <kyleeharward21@gmail.com>
2022-04-07 17:14:18 -0600

    class4.7

--- pokemon/pokemon.js
+++ pokemon/pokemon.js
@@ -7,4 +7,35 @@ const getAPIData = async (url) => {
         console.log(error)
     }
 }
-getAPIData('https://pokeapi.co/api/v2/pokemon/snorlax')
+
+const pokeGrid = document.querySelector('.pokegrid')
+
+async function loadPokemon(offset, limit){
+    const data= await getAPIData('https://pokeapi.co/api/v2/pokemon/snorlax')
+    populatePokeGrid(data)
+}
+
+function populatePokeGrid(pokemonArray){
+//loop through array and populate individual pokemon cards 
+    populatePokeCard(pokemonArray[0])
+}
+
+function populatePokeCard(pokemon){
+    const pokeScene = document.createElement('div')
+    pokeScene.className = 'scene'
+    const pokeCard = document.createElement('div')
+    pokeCard.className = 'card'
+    pokeCard,addEventListener('click', () => pokeCard.classList.toggle('is-flipped'))
+    //populate the front of the card
+
+    pokeScene.appendChild(pokeCard)
+    pokeGrid.appendChild(pokeScene)
+}
+
+function populateCardFront(pokemon){
+    const pokeFront = document.createElement('figure')
+    pokeFront.className = '.cardFace'
+    const pokeImg = document.createElement('img')
+
+}
+loadPokemon(0, 0)
