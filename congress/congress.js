1ed41adbdb062a17626a01d682d9c42efdc995ba
Kylee Harward <kyleeharward21@gmail.com>
2022-04-06 18:36:21 -0600

    added basic 2 buttons

--- congress/congress.js
+++ congress/congress.js
@@ -1,9 +1,14 @@
+//import section
 import { senators } from '../data/senators.js'
 import { representatives } from '../data/representatives.js'
+import { removeChildren } from '../utils/index.js';
 
-const senatorsDiv = document.querySelector('.senatorsDiv')
-const representativesDiv = document.querySelector('.representativesDiv')
+//declaring variables for document
+const main = document.querySelector("main");
+const congressDiv = document.querySelector('.congressDiv')
+const header = document.querySelector("header");
 
+//senators all function
 function simplifiedSenators() {
   return senators.map(sen => {
     const middleName = sen.middle_name ? ` ${sen.middle_name} ` : ` `
@@ -18,6 +23,44 @@ function simplifiedSenators() {
       missedVotesPct: sen.missed_votes_pct,
       loyaltyPct: sen.votes_with_party_pct
     }
+  })
+}
+function populateSenatorsDiv(senatorsArray) {
+  removeChildren(congressDiv)
+  senatorsArray.forEach(sen => {
+    const senFigure = document.createElement('figure')
+    const senImg = document.createElement('img')
+    const senCaption = document.createElement('figcaption')
+    senImg.src = sen.imgURL
+    senCaption.textContent = sen.name
+    senFigure.appendChild(senImg)
+    senFigure.appendChild(senCaption)
+    congressDiv.appendChild(senFigure)
+  })
+}
+//senator button
+const senButton = document.createElement("button");
+senButton.textContent = "Senators";
+senButton.addEventListener("click", function () {
+  populateSenatorsDiv(simplifiedSenators())
+});
+header.appendChild(senButton);
+
+//representatives function 
+
+function populateRepresentativesDiv(representativesArray) {
+  removeChildren(congressDiv)
+  representativesArray.forEach(rep => {
+    const repFigure = document.createElement('figure')
+    const repImg = document.createElement('img')
+    const repCaption = document.createElement('figcaption')
+
+    repImg.src = rep.imgURL
+    repCaption.textContent = rep.name
+
+    repFigure.appendChild(repImg)
+    repFigure.appendChild(repCaption)
+    congressDiv.appendChild(repFigure)
   })
 }
 function simplifiedRepresentatives() {
@@ -36,34 +79,12 @@ function simplifiedRepresentatives() {
     }
   })
 }
-function populateSenatorsDiv(senatorsArray) {
-  senatorsArray.forEach(sen => {
-    const senFigure = document.createElement('figure')
-    const senImg = document.createElement('img')
-    const senCaption = document.createElement('figcaption')
 
-    senImg.src = sen.imgURL
-    senCaption.textContent = sen.name
+//representatives button
+const repButton = document.createElement("button");
+repButton.textContent = "Representatives";
+repButton.addEventListener("click", function () {
+  populateRepresentativesDiv(simplifiedRepresentatives())
+});
+header.appendChild(repButton);
 
-    senFigure.appendChild(senImg)
-    senFigure.appendChild(senCaption)
-    senatorsDiv.appendChild(senFigure)
-  })
-}
-function populateRepresentativesDiv(representativesArray) {
-  representativesArray.forEach(rep => {
-    const repFigure = document.createElement('figure')
-    const repImg = document.createElement('img')
-    const repCaption = document.createElement('figcaption')
-
-    repImg.src = rep.imgURL
-    repCaption.textContent = rep.name
-
-    repFigure.appendChild(repImg)
-    repFigure.appendChild(repCaption)
-    representativesDiv.appendChild(repFigure)
-  })
-}
-
-populateSenatorsDiv(simplifiedSenators())
-populateRepresentativesDiv(simplifiedRepresentatives())
