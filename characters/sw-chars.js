442c3397333591db99eff97f3422d0873f2d3d86
Kylee Harward <kyleeharward21@gmail.com>
2022-04-27 20:18:18 -0600

    added more style and functionality to sw chars

--- characters/sw-chars.js
+++ characters/sw-chars.js
@@ -1,14 +1,13 @@
 import { people } from "../data/people.js";
 import { getLastNumber, removeChildren } from "../utils/index.js";
 
-const header = document.querySelector("header");
-const main = document.querySelector("main");
+const filterButtons = document.querySelector(".filterButtons")
+const characterDiv = document.querySelector(".characterDiv")
+const displayText = document.querySelector(".displayText")
 
 const allCharsButton = document.createElement("button");
 allCharsButton.textContent = "All Characters";
-allCharsButton.addEventListener("click", function () {
-  populateDOM(people);
-});
+allCharsButton.addEventListener("click", function () {populateDOM(people)});
 
 const maleCharacters = people.filter((person) => person.gender == "male");
 const maleCharsButton = document.createElement("button");
@@ -18,22 +17,21 @@ maleCharsButton.addEventListener("click", () => populateDOM(maleCharacters));
 const femaleCharacters = people.filter((person) => person.gender == "female");
 const femaleCharsButton = document.createElement("button");
 femaleCharsButton.textContent = "Female Characters";
-femaleCharsButton.addEventListener("click", () =>
-  populateDOM(femaleCharacters)
-);
+femaleCharsButton.addEventListener("click", () => populateDOM(femaleCharacters));
 
 const otherCharacters = people.filter((person) =>person.gender !== "male" && person.gender !== "female");
 const otherCharsButton = document.createElement("button");
 otherCharsButton.textContent = "Other Characters";
 otherCharsButton.addEventListener("click", () => populateDOM(otherCharacters));
 
-header.appendChild(allCharsButton);
-header.appendChild(maleCharsButton);
-header.appendChild(femaleCharsButton);
-header.appendChild(otherCharsButton);
+filterButtons.appendChild(allCharsButton);
+filterButtons.appendChild(maleCharsButton);
+filterButtons.appendChild(femaleCharsButton);
+filterButtons.appendChild(otherCharsButton);
+
 
 function populateDOM(characters) {
-  removeChildren(main);
+  removeChildren(characterDiv);
   characters.forEach((person) => {
     const personFig = document.createElement("figure");
     const personImg = document.createElement("img");
@@ -44,7 +42,7 @@ function populateDOM(characters) {
 
     personFig.appendChild(personImg);
     personFig.appendChild(personCap);
-    main.appendChild(personFig);
+    characterDiv.appendChild(personFig);
   });
 }
 populateDOM(people)
